5941e41678010b22dee345d973420fd59b2c6ecc
graphine <thesixthprime@gmail.com>
2021-05-27 13:26:02 +0530

    added auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import Hello from './components/Hello';
 import Greet from './components/Greet';
+import Amplify from '@aws-amplify/core';
 
 function App() {
   return (
@@ -8,8 +10,10 @@ function App() {
       <Hello />
       <Greet name={1 + 1} heroName='Superman' />
       <p>I'm Mithilesh.</p>
+      <h1>We now have Auth!</h1>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
